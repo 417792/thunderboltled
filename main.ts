f583d3efc2dad23bcdc72533949fdcbf37e5c0ba
417792 <115142684+417792@users.noreply.github.com>
2022-11-17 16:47:51 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,22 @@
-input.onButtonPressed(Button.A, function () {
-    if (x == 4) {
-        a = -1
-    } else if (x == 4) {
-        a = -1
-    }
-    dx += a
-    dx2 += a
-    dx3 += a
-    dx4 += a
-    led.plotBrightness(dx, 0, 255)
-    led.plotBrightness(dx2, 0, 193)
-    led.plotBrightness(dx3, 0, 146)
-    led.plotBrightness(dx4, 0, 23)
-})
-let dx3 = 0
-let dx4 = 0
-let dx2 = 0
-let dx = 0
-let a = 0
-let x = 0
-x = 0
 let y = 0
-a = 0
-dx = 0
-dx2 = 1
-dx2 = 2
-dx4 = 3
+let x = 2
+let dx = 1
 basic.forever(function () {
+	
+})
+basic.forever(function () {
+    y = 0
     if (x == 4) {
-        a = -1
+        dx = -1
     } else if (x == 0) {
-        a = 1
+        dx = 1
     }
-    x += a
-    led.plot(x, 0)
-    led.plot(x, 1)
-    led.plot(x, 2)
-    led.plot(x, 3)
-    led.plot(x, 4)
+    for (let index = 0; index < 5; index++) {
+        led.plotBrightness(dx - 0, y, 20)
+        led.plotBrightness(dx - 1, y, 60)
+        led.plotBrightness(dx + 1, y, 225)
+        y += 1
+    }
     basic.pause(100)
     basic.clearScreen()
 })
